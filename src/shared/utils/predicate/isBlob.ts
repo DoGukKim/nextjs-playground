10d0bef2bf0494f8746d720160f7e3182c63b861
DoGukKim <doguk1994@gmail.com>
2025-12-10 21:30:17 +0900

    refactor: isBlob 로직 수정

--- src/shared/utils/predicate/isBlob.ts
+++ src/shared/utils/predicate/isBlob.ts
@@ -1,4 +1,3 @@
 export function isBlob(value: unknown): value is Blob {
-  const tag = Object.prototype.toString.call(value);
-  return tag === "[object Blob]" || tag === "[object File]";
+  return value instanceof Blob;
 }
