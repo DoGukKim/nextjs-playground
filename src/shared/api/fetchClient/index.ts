3bafeb5fc3a2358eb442193aa35a02837fcc0f93
DoGukKim <doguk1994@gmail.com>
2025-12-11 11:25:33 +0900

    feat: fetchClient index report 생성

---
+++ src/shared/api/fetchClient/index.ts
@@ -0,0 +1,12 @@
+export { default as FetchClient } from "./fetchClient";
+export { default } from "./fetchClient";
+
+export { default as FetchClientError } from "./fetchClientError";
+
+export type {
+  FetchClientConfig,
+  FetchClientRequestConfig,
+  FetchClientHooks,
+  FetchClientErrorConfig,
+  ResponseType,
+} from "./types";
